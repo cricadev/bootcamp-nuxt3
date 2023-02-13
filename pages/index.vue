e79231999bffac4a9415d6e9ecba9e56b9e26160
CricaDev <cristiancasallas998@gmail.com>
2023-02-12 21:31:30 -0500

    new meta description

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,8 @@ useHead({
     {
       hid: "description",
       name: "description",
-      content: "Car",
+      content:
+        "Find the perfect car for you on our ecommerce platform, where you can easily publish your car for sale and receive competitive offers. Buy or sell your car with ease and confidence.",
     },
   ],
 });
